dff49124bf6a37436a267653d210f3249b4fb30e
Brian Perez <23-01884@g.batstate-u.edu.ph>
2025-09-11 16:34:06 +0800

    fix: remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-
+      debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => HomeScreen(),
         '/exercises': (context) => ExercisePage(),
